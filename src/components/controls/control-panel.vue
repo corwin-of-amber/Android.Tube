b5eb6c2a4d94e4eef754fca963c8e657fdbc6327
Shachar Itzhaky <corwin.amber@gmail.com>
2024-10-07 17:04:40 +0300

    [feature] Sleep timer toggle.

--- src/components/controls/control-panel.vue
+++ src/components/controls/control-panel.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="control-panel" :class="{expand}">
         <div class="controls">
-            <sleep-timer v-model:state="state.sleep"/>
+            <sleep-timer :state="state.sleep" @toggle="sleepToggle"/>
             <!-- <playlist-button ref="playlist" :show="show"/> -->
             <play-pause-button ref="playPause"/>
             <position-bar ref="position"/>
@@ -16,39 +16,56 @@
 }
 </style>
 
-<script>
+<script lang="ts">
+import { Vue, Component, Prop, toNative } from 'vue-facing-decorator';
 import PlayPauseButton from './play-pause-button.vue';
 import PositionBar from './position-bar.vue';
 import SleepTimer from './sleep-timer.vue'
 
-export default {
-    props: ['state', 'show'],
-    data: function() { return {expand: true, status: {}, monitorInterval: 500}; },
-    methods: {
-        toggle() {
-            this.expand = !this.expand;
-            this.expand ? this.monitor() : this.unmonitor();
-        },
-        monitor() {
-            if (this._monitor) return;
-            var h;
-            this._monitor = setInterval(h = () => {
-                //controls.getStatus(s => { this.status = s; });
-            }, this.monitorInterval);
-            h();
-        },
-        unmonitor() {
-            if (this._monitor) {
-                clearInterval(this._monitor);
-                this._monitor = null;
-            }
-        }
-    },
-    mounted() {
-        if (this.expand) this.monitor();
-    },
+@Component({
     components: {
         PlayPauseButton, PositionBar, SleepTimer
     }
+})
+export class IControlPanel extends Vue {
+    @Prop state: any
+    @Prop show: any
+
+    expand = true
+    status = {}
+    monitorInterval = 500
+
+    toggle() {
+        this.expand = !this.expand;
+        this.expand ? this.monitor() : this.unmonitor();
+    }
+
+    monitor() {
+        if (this._monitor) return;
+        var h;
+        this._monitor = setInterval(h = () => {
+            //controls.getStatus(s => { this.status = s; });
+        }, this.monitorInterval);
+        h();
+    }
+    unmonitor() {
+        if (this._monitor) {
+            clearInterval(this._monitor);
+            this._monitor = null;
+        }
+    }
+
+    sleepToggle() {
+        if (this.state.sleep.isRunning)
+            this.state.sleep.stop();
+        else
+            this.state.sleep.start();
+    }
+
+    mounted() {
+        if (this.expand) this.monitor();
+    }
 }
+
+export default toNative(IControlPanel)
 </script>
--- src/components/controls/sleep-timer.vue
+++ src/components/controls/sleep-timer.vue
@@ -1,11 +1,13 @@
 <template>
     <span class="sleep-timer">
-        <span class="indicator" :class="{isRunning: state.isRunning}">🌙</span>
+        <span class="indicator" @click="$emit('toggle')"
+              :class="{isRunning: state.isRunning}">🌙</span>
         <span class="mins">{{ state.mins }}</span>
     </span>
 </template>
 
 <style scoped>
+.sleep-timer { cursor: pointer; }
 .indicator { margin-right: .3em; }
 .indicator:not(.isRunning) {
     color: transparent;
@@ -17,7 +19,9 @@
 <script lang="ts">
 import { Vue, Component, Prop, toNative } from 'vue-facing-decorator';
 
-@Component
+@Component({
+    emits: ['toggle', 'reset']
+})
 export class ISleepTimer extends Vue {
     @Prop
     state = {mins: 45, isRunning: false }
